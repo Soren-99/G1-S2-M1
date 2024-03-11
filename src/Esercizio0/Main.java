16aca10c4428c5f53abc00ea42360198f88b6fb2
Persiano99 <sorenrahimi99@gmail.com>
2024-03-11 18:10:08 +0100

    Esercizio 1

--- src/Esercizio0/Main.java
+++ src/Esercizio0/Main.java
@@ -1,2 +1,33 @@
-package Esercizio0;public class Main {
+package Esercizio0;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Inserisci un numero:");
+        int numero = 0;
+        try {
+            numero = scanner.nextInt();
+        } catch (Exception e) {
+            System.out.println("Errore: inserire un numero valido.");
+            return;
+        }
+
+        int risultato = calcolaQuadrato(numero);
+        System.out.println("Il quadrato di " + numero + " è: " + risultato);
+    }
+
+    public static int calcolaQuadrato (int numero) {
+        try{
+            if (numero < 0) {
+                throw new IllegalAccessException("Numero negativo non consentito.");
+            }
+            return numero * numero;
+        } catch (IllegalAccessException e) {
+            System.out.println("Errore: " + e.getMessage());
+            return -1;
+        }
+    }
 }
--- src/Esercizio1/Main.java
+++ src/Esercizio1/Main.java
@@ -1,2 +1,53 @@
-package Esercizio1;public class Main {
+package Esercizio1;
+
+import java.util.Scanner;
+import java.util.Random;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int[] array = new int[5];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(10) +1;
+        }
+
+        System.out.print("Array originale: ");
+        stampaArray(array);
+
+        while (true){
+            System.out.print("/nInserisci un numero (0 per terminare): ");
+            int numero = scanner.nextInt();
+
+            if (numero == 0) {
+                System.out.println("Arrivederci");
+                break;
+            }
+
+            System.out.println("Inserisci la posizione (0-4): ");
+            int posizione = scanner.nextInt();
+
+            try {
+                if(posizione < 0 || posizione >= array.length) {
+                    throw new ArrayIndexOutOfBoundsException("Posizione non valida");
+                }
+                array[posizione] = numero;
+
+                System.out.print("Nuovo stato dell'array: ");
+                stampaArray(array);
+            }catch (ArrayIndexOutOfBoundsException e){
+                System.out.println("Errore " + e.getMessage());
+            }
+        }
+
+        scanner.close();
+    }
+
+    public static void stampaArray(int[] array){
+        System.out.print("[ ");
+        for (int i = 0; i < array.length; i++){
+            System.out.print(array[i] + " ");
+        }
+        System.out.print("]");
+    }
 }
